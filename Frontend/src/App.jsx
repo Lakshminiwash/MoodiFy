602e2982bae28f697f2e2f2379a429692d235402
Lakshminiwash <lakshminiwash07rr@gmail.com>
2026-09-24 09:00:12 +0530

    another fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,21 +3,19 @@ import "./features/shared/style/globle.scss"
 import { router } from "./appRoutes"
 import { AuthProvider } from "./features/auth/auth.Context"
 import { SongContextProvider } from "./features/home/Song.context"
-import { useAuth } from "./features/auth/hooks/useAuth"
-import { useEffect } from "react"
 
-const App = () => {
-  const auth = useAuth()
-  useEffect(()=>{
-    auth.handleGetMe()
-  },[])
+const AppContent = () => {
   return (
-    <AuthProvider>
-      <SongContextProvider>
-        <RouterProvider router={router} />
-      </SongContextProvider>
-    </AuthProvider>
+    <SongContextProvider>
+      <RouterProvider router={router} />
+    </SongContextProvider>
   )
 }
+
+const App = () => (
+  <AuthProvider>
+    <AppContent />
+  </AuthProvider>
+)
 
 export default App
