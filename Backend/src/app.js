15071a9d4d2306a6321b0aab9f76e6563a409e9c
Lakshminiwash <lakshminiwash07rr@gmail.com>
2026-09-24 09:24:26 +0530

    fix the get-me feature

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,30 +2,24 @@ const express = require("express")
 const app = express();
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
-// const path = require("path")
 
 
-// app.use(express.static(path.join(__dirname, "../public")))
 app.use(express.static("./public"))
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:[
+    origin: [
         "http://localhost:3000",
-        "https://moodify-9wun.onrender.com"
     ],
-    credentials:true
+    credentials: true
 }))
 
 
 const authRoutes = require("./Routes/auth.Routes")
 const songRoutes = require("./Routes/song.routes")
 
-app.use("/api/auth",authRoutes)
-app.use("/api/songs",songRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/songs", songRoutes)
 
-// app.use((req, res) => {
-//    res.sendFile(path.join(__dirname, "../public", "index.html"))
-// })
 
 module.exports = app
